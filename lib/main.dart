31d1724684a0da190c150fdcec239009c2082412
soniyanw <soniya@nativewit.in>
2022-08-01 17:45:31 +0530

    Animations added

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,94 @@
-// @dart=2.9
+import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() => runApp(MaterialApp(
-      home: MyApp(),
-    ));
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
-class _MyAppState extends State<MyApp> {
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late AnimationController _animationController;
+  late Animation color;
+  int dir = 0;
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2))
+          ..repeat();
+    color = ColorTween(begin: Colors.pink[100], end: Colors.white).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.bounceOut));
+    _animationController.addListener(() {
+      setState(() {});
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MyModel>(
-      create: (context) => MyModel(),
-      child: MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('PROVIDER'),
-              centerTitle: true,
-            ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    child: Text("SELECT SHAPE AND PRESS THE BUTTON"),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(35),
-                    color: Colors.white,
-                    child: Selector<MyModel, Icon>(
-                      selector: (buildContext, myModel) => myModel.center,
-                      builder: (context, center, child) {
-                        return FittedBox(
-                          child: center,
-                          fit: BoxFit.fitWidth,
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            floatingActionButton:
-                Consumer<MyModel>(builder: (context, myModel, child) {
-              return MaterialButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    myModel.doSomething(_selectedIndex);
-                  },
-                  child: Text(
-                    "Change shape",
-                    style: TextStyle(color: Colors.white),
-                  ));
-            }),
-            bottomNavigationBar: BottomNavigationBar(
-              items: [
-                BottomNavigationBarItem(icon: Icon(Icons.mood), label: "Happy"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.cloud), label: "Cloud"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.sunny), label: "Sunny"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.diamond), label: "Diamond"),
-              ],
-              currentIndex: _selectedIndex,
-              selectedItemColor: Colors.blue,
-              unselectedItemColor: Colors.grey,
-              onTap: _onItemTapped,
-            )),
-      ),
-    );
+  void dispose() {
+    _controller.dispose();
+    _animationController.dispose();
+    super.dispose();
   }
-}
 
-class MyModel with ChangeNotifier {
-  Icon center = Icon(Icons.mood);
-  void doSomething(int index) {
-    switch (index) {
-      case 0:
-        center = Icon(Icons.mood);
-        break;
-      case 1:
-        center = Icon(Icons.cloud);
-        break;
-      case 2:
-        center = Icon(Icons.sunny);
-        break;
-      case 3:
-        center = Icon(Icons.diamond);
-        break;
-    }
-    notifyListeners();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          title: Text("Animation"),
+        ),
+        body: AnimatedBuilder(
+          animation: _animationController,
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: color.value,
+            child: AnimatedBuilder(
+              animation: _controller,
+              child: Container(
+                width: 200.0,
+                height: 200.0,
+                child: Image(image: AssetImage("assets/images.jpeg")),
+              ),
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: dir == 1
+                      ? _controller.value * 2 * pi
+                      : _controller.value * -2 * pi,
+                  child: child,
+                );
+              },
+            ),
+          ),
+          builder: (context, child) {
+            return Container(
+              child: child,
+            );
+          },
+        ),
+        floatingActionButton: MaterialButton(
+          color: Colors.pink,
+          child: Text(
+            "Change Direction",
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            dir = dir == 1 ? 0 : 1;
+            setState(() {});
+          },
+        ));
   }
 }
---
+++ lib/provider.dart
@@ -0,0 +1,112 @@
+// @dart=2.9
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MyModel>(
+      create: (context) => MyModel(),
+      child: MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('PROVIDER'),
+              centerTitle: true,
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    child: Text("SELECT SHAPE AND PRESS THE BUTTON"),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(35),
+                    color: Colors.white,
+                    child: Selector<MyModel, Icon>(
+                      selector: (buildContext, myModel) => myModel.center,
+                      builder: (context, center, child) {
+                        return FittedBox(
+                          child: center,
+                          fit: BoxFit.fitWidth,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            floatingActionButton:
+                Consumer<MyModel>(builder: (context, myModel, child) {
+              return MaterialButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    myModel.doSomething(_selectedIndex);
+                  },
+                  child: Text(
+                    "Change shape",
+                    style: TextStyle(color: Colors.white),
+                  ));
+            }),
+            bottomNavigationBar: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(icon: Icon(Icons.mood), label: "Happy"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.cloud), label: "Cloud"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.sunny), label: "Sunny"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.diamond), label: "Diamond"),
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.blue,
+              unselectedItemColor: Colors.grey,
+              onTap: _onItemTapped,
+            )),
+      ),
+    );
+  }
+}
+
+class MyModel with ChangeNotifier {
+  Icon center = Icon(Icons.mood);
+  void doSomething(int index) {
+    switch (index) {
+      case 0:
+        center = Icon(Icons.mood);
+        break;
+      case 1:
+        center = Icon(Icons.cloud);
+        break;
+      case 2:
+        center = Icon(Icons.sunny);
+        break;
+      case 3:
+        center = Icon(Icons.diamond);
+        break;
+    }
+    notifyListeners();
+  }
+}
